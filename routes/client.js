4c9fbbf33a5745c1e531aefd7478b04eac33069e
willian-pessoa <willianpessoadeabreu@gmail.com>
2023-04-19 22:21:01 -0300

    create get customers route

--- routes/client.js
+++ routes/client.js
@@ -1,8 +1,9 @@
 import express from "express"
-import { getProducts } from "../controllers/client.js"
+import { getProducts, getCostumers } from "../controllers/client.js"
 
 const router = express.Router()
 
 router.get("/products", getProducts)
+router.get("/customers", getCostumers)
 
 export default router
