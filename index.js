fd71b990d3549bb2f444be65ddbb7aa30664653a
willian-pessoa <willianpessoadeabreu@gmail.com>
2023-04-25 15:30:51 -0300

    add sales route

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ import User from "./models/User.js"
 import Product from "./models/Product.js"
 import ProductStat from "./models/ProductStat.js"
 import Transaction from "./models/Transaction.js"
-import { dataUser, dataProduct, dataProductStat, dataTransaction } from "./data/index.js"
+import OverallStat from "./models/OverallStat.js"
+import { dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat } from "./data/index.js"
 
 // CONFIGURATIONS
 dotenv.config()
@@ -47,4 +48,5 @@ mongoose.connect(process.env.MONGO_URL, {
   //ProductStat.insertMany(dataProductStat)
   //Transaction.insertMany(dataTransaction)
   //User.insertMany(dataUser)
+  //OverallStat.insertMany(dataOverallStat)
 }).catch((error) => console.log(`${error} did not connect`))
