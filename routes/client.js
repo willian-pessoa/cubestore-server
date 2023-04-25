98d688cfed4594fe00b1a37117306a0ac15479f5
willian-pessoa <willianpessoadeabreu@gmail.com>
2023-04-25 12:56:26 -0300

    add geography route

--- routes/client.js
+++ routes/client.js
@@ -1,10 +1,11 @@
 import express from "express"
-import { getProducts, getCostumers, getTransactions } from "../controllers/client.js"
+import { getProducts, getCostumers, getTransactions, getGeography } from "../controllers/client.js"
 
 const router = express.Router()
 
 router.get("/products", getProducts)
 router.get("/customers", getCostumers)
 router.get("/transactions", getTransactions)
+router.get("/geography", getGeography)
 
 export default router
