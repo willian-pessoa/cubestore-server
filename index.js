b7beb9e5d474a10cfbf67b2a89967810cf306f58
willian-pessoa <willianpessoadeabreu@gmail.com>
2023-05-02 11:56:57 -0300

    inserido dados afiliados no db

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ import Product from "./models/Product.js"
 import ProductStat from "./models/ProductStat.js"
 import Transaction from "./models/Transaction.js"
 import OverallStat from "./models/OverallStat.js"
-import { dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat } from "./data/index.js"
+import AffiliateStat from "./models/AffiliateStat.js"
+import { dataUser, dataProduct, dataProductStat, dataTransaction, dataOverallStat, dataAffiliateStat } from "./data/index.js"
 
 // CONFIGURATIONS
 dotenv.config()
@@ -49,4 +50,5 @@ mongoose.connect(process.env.MONGO_URL, {
   //Transaction.insertMany(dataTransaction)
   //User.insertMany(dataUser)
   //OverallStat.insertMany(dataOverallStat)
+  //AffiliateStat.insertMany(dataAffiliateStat)
 }).catch((error) => console.log(`${error} did not connect`))
